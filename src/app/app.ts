a426f53f017c11402ac45c43ecd3bd0b484c78b5
raul-hora <neckromar4@gmail.com>
2026-03-27 15:39:44 +0100

    Correccion nombre clase para despligue

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import { RecaptchaModule } from 'ng-recaptcha'; 
+import { CommonModule } from '@angular/common'; 
+import { RecaptchaModule } from 'ng-recaptcha';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RecaptchaModule], 
+  imports: [CommonModule, RecaptchaModule],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
-export class AppComponent {
+export class App {
   captchaResuelto: boolean = false;
 
   // Se ejecuta cuando el usuario marca el "No soy un robot"
